046e47df346532b8dc301242da328056618367d0
Rui Filipe <162214885+Rui6filipe@users.noreply.github.com>
2025-03-20 18:19:29 +0000

    Update BNN_Model.py

--- BNN_Model.py
+++ BNN_Model.py
@@ -10,12 +10,11 @@ import numpy as np
 from xgboost import XGBRegressor
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 import seaborn as sns
 import matplotlib.pyplot as plt
 from datetime import datetime
 import warnings
-from sklearn.mixture import GaussianMixture
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -24,12 +23,8 @@ import copy
 import optuna
 from torch.optim.lr_scheduler import OneCycleLR
 from sklearn.ensemble import RandomForestRegressor
-from scipy.stats import pearsonr
 from functools import partial
-from scipy.stats import gaussian_kde 
 from scipy.stats import norm
-from properscoring import crps_gaussian
-from numba import njit
 
 # Suppress specific warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -103,7 +98,6 @@ class BayesianNN(nn.Module):
     def __init__(self, input_size, params):
         super().__init__()
         
-        # Extract hyperparameters from params dictionary
         hidden_size1 = params.get('units_layer1', 128)
         hidden_size2 = params.get('units_layer2', 64)
         hidden_size3 = params.get('units_layer3', 32)
@@ -113,9 +107,8 @@ class BayesianNN(nn.Module):
             nn.Linear(input_size, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(),
-            nn.Dropout(0)
-        )
-        # Bayesian layers
+            nn.Dropout(0))
+        
         self.bayesian_layer1 = BayesianLinear(input_size, hidden_size1)
         self.bayesian_layer2 = BayesianLinear(hidden_size1, hidden_size2)
         self.bayesian_layer3 = BayesianLinear(hidden_size2, hidden_size3)
@@ -131,7 +124,7 @@ class BayesianNN(nn.Module):
         return x
     
 
-    def sample_elbo(self, input, target, loss_fn, sample_weights, kl_weight=0, num_samples=100, log = False):
+    def sample_elbo(self, input, target, loss_fn, sample_weights, kl_weight=0, num_samples=100):
         """
         Compute the Evidence Lower Bound (ELBO) loss for a Bayesian Neural Network.
         """      
@@ -147,7 +140,6 @@ class BayesianNN(nn.Module):
         pred_mean = predictions.mean(dim=0)
         pred_var = predictions.var(dim=0) + 1e-8
 
-        #nll = 0.5 * torch.log(2 * torch.pi * pred_var) + ((target - pred_mean) ** 2) / (2 * pred_var)
         nll = crps_gaussian_torch(target, pred_mean, torch.sqrt(pred_var))
  
         prediction_loss = (nll * sample_weights).mean()
@@ -160,11 +152,6 @@ class BayesianNN(nn.Module):
             self.bayesian_output.kl_loss())
         
         total_loss = prediction_loss + kl_weight * kl_div
-        
-        #if log:
-        #    print("\nPrediction Loss :", prediction_loss)
-        #    print("KL Loss :", kl_weight * kl_div)
-        #    print("Components :", kl_div)
     
         return total_loss
   
@@ -178,34 +165,6 @@ def crps_gaussian_torch(target, mean, std):
     crps = std * (standardized * (2 * cdf - 1) + 2 * pdf - 1 / torch.sqrt(torch.tensor(torch.pi)))
     return crps
 
-
-def betting_profit_loss_torch(actual, pred_mean, pred_std, odds=1.66, threshold1=6.5, threshold2=7.5):
-    """
-    Loss function that optimizes for Kelly Criterion-based expected profit.
-    Penalizes bad bets and rewards profitable ones.
-    """
-    #thresholds = torch.tensor(6.5, dtype=torch.float32, device=actual.device)
-    thresholds = torch.full_like(actual, 6.5, dtype=torch.float32)
-    #thresholds = torch.where(actual > 7.5, torch.tensor(threshold2, device=actual.device), torch.tensor(threshold1, device=actual.device))
-    
-    normal_dist = torch.distributions.Normal(pred_mean, pred_std)
-    prob_under = normal_dist.cdf(thresholds)
-
-    prob_over = 1 - prob_under  
-    best_prob = torch.maximum(prob_over, prob_under)
-    implied_prob = 1 / odds
-    payout = odds - 1
-
-    edge = best_prob - implied_prob  
-    #kelly_fraction = torch.clamp(edge / payout, 0)
-    kelly_fraction = torch.sigmoid(edge / payout) 
-
-    bet_won = torch.sigmoid((prob_over - prob_under) * (actual - thresholds))
-    #bet_won = (prob_over > prob_under) == (actual > thresholds)
-
-    profit = kelly_fraction * (bet_won * payout - (1 - bet_won))
-
-    return (1 - profit)
 
 #%%
 
@@ -355,14 +314,13 @@ def plot_correlation_heatmap(df, features):
     
     sns.heatmap(
         correlation_matrix,
-        annot=True,          # Show numbers
-        cmap='RdBu',        # Red-Blue diverging colormap
-        center=0,           # Center the colormap at 0
-        fmt='.2f',         # Show 2 decimal places
-        square=True,       # Make cells square
-        annot_kws={'size': 7},  # Make numbers a bit smaller
-        cbar_kws={'shrink': .8}  # Slightly smaller colorbar
-    ) 
+        annot=True,          
+        cmap='RdBu',       
+        center=0,          
+        fmt='.2f',         
+        square=True,     
+        annot_kws={'size': 7},  
+        cbar_kws={'shrink': .8}) 
     
     plt.xticks(rotation=45, ha='right')
     plt.yticks(rotation=0)
@@ -389,7 +347,7 @@ def train_test_split(df, n_ignore, n_test):
     remove_indices = []
     for season in seasons:
         season_df = df[df["team1"].str.endswith(season)]
-        season_indices = season_df.head(n_ignore).index  # First n games for this season
+        season_indices = season_df.head(n_ignore).index  
         remove_indices.extend(season_indices)
     df = df.drop(remove_indices)
     
@@ -424,7 +382,7 @@ def validation_split(df, n_ignore, n_test, n_prev_season):
     remove_indices = []
     for season in seasons:
         season_df = df[df["team1"].str.endswith(season)]
-        season_indices = season_df.head(n_ignore).index  # First n games for this season
+        season_indices = season_df.head(n_ignore).index 
         remove_indices.extend(season_indices)
     df = df.drop(remove_indices)
     
@@ -454,7 +412,7 @@ def create_time_weights(dates):
     """
     Create weights that give more importance to recent seasons
     """
-    '''season_ranges = [ 
+    season_ranges = [ 
         (datetime(2024, 9, 1), datetime(2025, 9, 1), 10),
         (datetime(2023, 9, 1), datetime(2024, 9, 1), 2),
         (datetime(2022, 9, 1), datetime(2023, 9, 1), 2),
@@ -463,21 +421,9 @@ def create_time_weights(dates):
         (datetime(2019, 9, 1), datetime(2020, 9, 1), 1),
         (datetime(2018, 9, 1), datetime(2019, 9, 1), 1),
         (datetime(2017, 9, 1), datetime(2018, 9, 1), 1),
-        (datetime(2016, 9, 1), datetime(2017, 9, 1), 1)]'''
-    
-    season_ranges = [ 
-        (datetime(2024, 9, 1), datetime(2025, 9, 1), 1),
-        (datetime(2023, 9, 1), datetime(2024, 9, 1), 1),
-        (datetime(2022, 9, 1), datetime(2023, 9, 1), 1),
-        (datetime(2021, 9, 1), datetime(2022, 9, 1), 1),
-        (datetime(2020, 9, 1), datetime(2021, 9, 1), 1),
-        (datetime(2019, 9, 1), datetime(2020, 9, 1), 1),
-        (datetime(2018, 9, 1), datetime(2019, 9, 1), 1),
-        (datetime(2017, 9, 1), datetime(2018, 9, 1), 1),
         (datetime(2016, 9, 1), datetime(2017, 9, 1), 1)]
     
     weights = []
-    # Check which season the game belongs to and assign the corresponding weight
     for date in dates:
         for start_date, end_date, season_weight in season_ranges:
             if start_date <= date < end_date:
@@ -499,7 +445,6 @@ def betting_profit_loss(actual, dist, odds=1.66, threshold1=6.5, threshold2=7.5)
     thresholds = np.where(actual > 7.5, threshold2, threshold1)
     
     prob_under = norm.cdf(thresholds, loc=pred_mean, scale=pred_std)
-    #prob_under = 1 / (1 + np.exp(-1.702 * (thresholds - pred_mean) / pred_std))
     prob_over = 1 - prob_under
 
     best_prob = np.maximum(prob_over, prob_under)
@@ -525,7 +470,6 @@ def get_baseline_score(train_test_splits, features):
     mae = []
     for i, (X_training, y_train, X_test, y_test) in enumerate(train_test_splits, start=1):
         
-        #weights = create_time_weights(X_training['game_date'])
         scaler = StandardScaler()
         X_train = scaler.fit_transform(X_training[features])
         X_test = scaler.transform(X_test[features])
@@ -541,7 +485,9 @@ def get_baseline_score(train_test_splits, features):
 
 
 def find_learning_rate(model, X_train, y_train, sample_weights, criterion, start_lr=1e-6, end_lr=1, num_iterations=1000):
-    """ Implementation of learning rate finder without batch processing """
+    """ 
+    Implementation of learning rate finder 
+    """
     lrs = []
     losses = []
     log_lrs = np.linspace(np.log10(start_lr), np.log10(end_lr), num_iterations)
@@ -603,7 +549,7 @@ def find_learning_rate(model, X_train, y_train, sample_weights, criterion, start
 
 def setup_lr(X_training, y_train, features, initial_params):
     """
-    Setup model and find maximum learning rate using FastAI's lr_find.
+    Setup model and find maximum learning rate.
     """
     weights = create_time_weights(X_training['game_date'])   
     sample_weights = torch.FloatTensor(weights)
@@ -661,7 +607,7 @@ def train(bnn_model, optimizer, criterion, X_train, y_train, sample_weights, sam
                 y_train, 
                 criterion, 
                 sample_weights,
-                kl_weight, log=(mode=="early_stop"))
+                kl_weight)
             
         else: 
             train_loss = bnn_model.sample_elbo(
@@ -703,7 +649,7 @@ def train(bnn_model, optimizer, criterion, X_train, y_train, sample_weights, sam
   
                 if epoch == 49 or epoch == 99 or epoch == 149 or epoch == 199 or epoch == 249 or epoch == 299:
                     val_loss = bnn_model.sample_elbo(X_val, y_val, criterion, 
-                                                   sample_weights_val, kl_weight, log = True)
+                                                   sample_weights_val, kl_weight)
                 else: 
                     val_loss = bnn_model.sample_elbo(X_val, y_val, criterion, 
                                                    sample_weights_val, kl_weight)
@@ -721,9 +667,6 @@ def train(bnn_model, optimizer, criterion, X_train, y_train, sample_weights, sam
             residuals_val = val_pred_mean - y_val.numpy().flatten()
             uncertainties_val = val_pred_uncertainty
 
-            #if (epoch + 1) % 50 == 0:
-                #print(f"Epoch {epoch + 1}/{num_epochs} - Train Loss = {train_loss.item():.4f} | Train NLL: {train_nll:.4f} | Train MAE: {train_mae:.4f} | Val Loss = {val_loss.item():.4f} | Val NLL: {val_nll:.4f} | Val MAE: {val_mae:.4f}")
-    
             if val_nll < best_val_loss and epoch>60:
                 best_val_loss = val_nll
                 epochs_without_improvement = 0
@@ -794,7 +737,7 @@ def train(bnn_model, optimizer, criterion, X_train, y_train, sample_weights, sam
 #%%
 def objective(optimal_lr, trial):
     """
-    Objective function for Bayesian optimization using Optuna.
+    Objective function for Bayesian optimization/
     """
     '''learning_rate = trial.suggest_uniform("learning_rate", optimal_lr/2, optimal_lr*3/2) 
     units_layer1 = trial.suggest_categorical("units_layer1", [128, 256])  
@@ -1080,10 +1023,8 @@ def plot_prediction_vs_actual(y_true, y_pred):
     """
     plt.figure(figsize=(10, 6))
     
-    # Create scatter plot
     plt.scatter(y_true, y_pred, alpha=0.5)
     
-    # Add perfect prediction line
     max_val = max(y_true.max(), y_pred.max())
     min_val = min(y_true.min(), y_pred.min())
     plt.plot([min_val, max_val], [min_val, max_val], 
@@ -1101,10 +1042,6 @@ def plot_test_results(target, predictions, distributions, thresholds=[5.5, 6.5, 
     """
     Create a standalone visualization of over/under performance.
     """
-    import matplotlib.pyplot as plt
-    import pandas as pd
-
-    # Compute over/under performance results
     over_under_results = []
     for threshold in thresholds:
         actual_over = (target > threshold).mean() 
@@ -1112,12 +1049,10 @@ def plot_test_results(target, predictions, distributions, thresholds=[5.5, 6.5, 
         over_under_results.append({
             'Threshold': threshold,
             'Actual Over %': actual_over * 100,
-            'Predicted Over %': pred_over * 100
-        })
+            'Predicted Over %': pred_over * 100})
 
     results_df = pd.DataFrame(over_under_results)
 
-    # Create bar plot
     x = range(len(thresholds))
     width = 0.35
     plt.figure(figsize=(10, 7))
@@ -1129,7 +1064,6 @@ def plot_test_results(target, predictions, distributions, thresholds=[5.5, 6.5, 
     plt.ylabel('Percentage of Games')
     plt.legend()
 
-    # Show the plot
     plt.tight_layout()
     plt.show()
     
@@ -1140,12 +1074,10 @@ def plot_prediction_distribution(distributions, actual, test_round_index, game_i
     """
     plt.figure(figsize=(10, 6))
     
-    # Plot histogram of predicted distribution
     sns.histplot(distributions[test_round_index][game_index], 
                 stat='probability',
                 discrete=True)
     
-    # Add vertical line for actual value if provided
     actual_value = actual[test_round_index].iloc[game_index]
     if actual_value is not None:
         plt.axvline(x=actual_value,  
@@ -1176,13 +1108,13 @@ plot_feature_importance(importance_df)
 plot_correlation_heatmap(df, selected_features)
 
 # Splits and baseline
-n_ignore = 63 #42
-n_test = 63-21 # 63
+n_ignore = 42
+n_test = 63-21 
 n_prev_season = 35
 n_iterations = 8
 validation_splits = validation_split(df, n_ignore, n_test, n_prev_season)
 train_test_splits = train_test_split(df, n_ignore, n_test)
-#get_baseline_score(train_test_splits, selected_features)
+get_baseline_score(train_test_splits, selected_features)
 
 # Tune hyperparameters
 initial_params = {
@@ -1191,11 +1123,11 @@ initial_params = {
     'units_layer3': 64,
     'units_layer4': 32}
 optimal_lr = 0.01
-#setup_lr(train_test_splits[0][0], train_test_splits[0][1], selected_features, initial_params)
+setup_lr(train_test_splits[0][0], train_test_splits[0][1], selected_features, initial_params)
 
-#best_params = tune_hyperparameters(validation_splits, selected_features, n_iterations, optimal_lr)
-best_params = {'learning_rate': 0.01, 'units_layer1': 128, 'units_layer2': 64,
-               'units_layer3': 16, 'units_layer4': 8, 'epochs': 150, 'kl_weight': 1.5e-6}
+best_params = tune_hyperparameters(validation_splits, selected_features, n_iterations, optimal_lr)
+'''best_params = {'learning_rate': 0.01, 'units_layer1': 128, 'units_layer2': 64,
+               'units_layer3': 16, 'units_layer4': 8, 'epochs': 150, 'kl_weight': 1.5e-6}'''
 print("\nBest parameters:", best_params)
 
 
